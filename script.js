17c65be783f94020c0281932cfd77ff9f31fa142
tumie <151614809+tumieK@users.noreply.github.com>
2024-10-17 12:07:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+let name="Hello there"
+name="haha"
+
+let areas=Math.pow(5,2);
+
+const person={
+  name: "Lerato",
+  surname: "Msholozi",
+}
+//Function
+printName:() =>  {
+  console.log("I'm a function")
+  
+} 
+  
+console.log(person)
+console.log(name)
+
